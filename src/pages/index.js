3fc2b030b1d3950cfab77b68338fd697528f9d8f
demowillage <104663285+demowillage@users.noreply.github.com>
2022-05-03 13:07:30 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,24 @@ import { Seo } from '../components/Seo'
 import { HomepageBanner } from '../components/HomepageBanner'
 import { MainContent } from '../components/MainContent'
 
+import { Slide1 } from '../components/HomePageSlider/Slide1'
+import { Slide2 } from '../components/HomePageSlider/Slide2'
+import { Slide3 } from '../components/HomePageSlider/Slide3'
+import { Slide4 } from '../components/HomePageSlider/Slide4'
+
 const Homepage = () => (
   <Layout isHomepage>
     <Seo title="Home" />
     <HomepageBanner />
-    <MainContent />
+    <br></br>
+    <Slide1 />
+    <br></br>
+    <Slide2 />
+    <br></br>
+    <Slide3 />
+    <br></br>
+    <Slide4 />
+    {/* <MainContent /> */}
   </Layout>
 )
 
