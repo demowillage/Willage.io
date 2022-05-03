cc8bd2962aa0fc5166d35b9e2e7aec8bded88633
demowillage <104663285+demowillage@users.noreply.github.com>
2022-05-03 13:27:46 +0530

    Add files via upload

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -10,13 +10,13 @@ export const HomepageBanner = () => (
       backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${homeBanner})`,
     }}
   >
-    <div className="banner-content container">
+    <div className="right-content container">
       <h2 className="banner-title">Willage.io</h2>
       <p className="banner-description">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
         Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
         Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede
-        asdf...
+        asdf.
       </p>
       <Link to="/about" className="banner-button">
         Learn More
