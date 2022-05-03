6c73a4615d6e48e4a84a1a812b4ad1c7fe839627
demowillage <104663285+demowillage@users.noreply.github.com>
2022-05-03 13:09:20 +0530

    Add files via upload

---
+++ src/components/HomePageSlider/Slide2.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import homeSliderBanner from '../../images/willage-2.png'
+
+export const Slide2 = () => (
+  <section
+    className="homepage-slider-banner"
+    style={{
+      backgroundImage: `url(${homeSliderBanner})`,
+    //   backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${homeBanner})`,
+    }}
+  >
+    <div className="slider-content container grid-container">
+      <div>
+        <br></br>
+        <div class="leftoverlay">
+          <h2 className="slider-title">Willage.io</h2>
+          <p className="slider-description">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+            Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
+            Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede
+            asdf.
+          </p>
+          <Link to="/about" className="banner-button">
+            Learn More
+          </Link>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+        <div class="upoverlay">
+          <div class="line"></div>
+        </div>
+    </div>
+    
+  </section>
+)
